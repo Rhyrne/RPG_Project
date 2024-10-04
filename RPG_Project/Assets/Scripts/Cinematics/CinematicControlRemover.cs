de5d2b1e8eb56e30fd9a22caf3989c5c5fca4862
Rhyrne <100185977+Rhyrne@users.noreply.github.com>
2024-10-04 23:43:48 +0300

    Cinemache used, Events added

---
+++ RPG_Project/Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,42 @@
+﻿using RPG.Controller;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        GameObject player;
+        void Start()
+        {
+            DisableDirector();
+            player = GameObject.FindWithTag("Player");
+            GetComponent<PlayableDirector>().played += Disabled;
+            GetComponent<PlayableDirector>().stopped += Enabled;
+            EnableDirector();
+        }
+
+        public void Enabled(PlayableDirector pd)
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+
+        public void Disabled(PlayableDirector pd)
+        {
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+
+        public void DisableDirector()
+        {
+            GetComponent<PlayableDirector>().enabled = false;
+        }
+
+        public void EnableDirector()
+        {
+            GetComponent<PlayableDirector>().enabled = true;
+        }
+    }
+}
